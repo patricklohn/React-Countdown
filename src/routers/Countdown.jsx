a5ab1167e8d45c761210fae757fd94093eeee7ff
Patrick Lohn <patricklohn3@gmail.com>
2024-10-26 16:49:41 -0300

    ajustando as datas

--- src/routers/Countdown.jsx
+++ src/routers/Countdown.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import Title from '../components/Title'
 import Counter from '../components/Counter'
 import useCountdown from '../hooks/useCountdown'
+import { useContext } from 'react'
+import { CountdownContext } from '../context/CountdownContext'
+
+import { Navigate } from 'react-router-dom'
 
 const Countdown = () => {
-    const [day, hour, minute, second] = useCountdown("Jan 1, 2025 00:00:00")
+    const {event} = useContext(CountdownContext);
+
+    if(!event) return <Navigate to="/"/>;
+
+    const eventTitle = event.title
+    const eventColor = event.color
+
+    const [day, hour, minute, second] = useCountdown(event.date)
 
     return (
     <>
-        <Title title="Contagem regressiva para 2025"/>
+        <Title title={eventTitle}/>
           <div className='countdown-container'>
             <Counter title={'Dias'} number={day}/>
             <Counter title={'Horas'} number={hour}/>
